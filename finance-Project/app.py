496e512cea385912c2b9faf7ab6f7b847f3d921f
DhanashriSaner <dhanashrisaner442@gmail.com>
2024-01-17 11:17:50 +0530

    final code

--- finance-Project/app.py
+++ finance-Project/app.py
@@ -5,12 +5,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 app = Flask(__name__)
 
 # Load your trained model
-model = joblib.load('./models/best_model.joblib')  # Replace with your actual model filename
+model = joblib.load('/home/dhanshree/Desktop/Project_Photos/finance-latest/best_model.joblib')  # Replace with your actual model filename
 
 # Load the TfidfVectorizer used during training
-vectorizer = joblib.load('./models/vectorizer.joblib')  # Replace with your actual vectorizer filename
+vectorizer = joblib.load('/home/dhanshree/Desktop/Project_Photos/finance-latest/vectorizer.joblib')  # Replace with your actual vectorizer filename
 
-encoder = joblib.load('./models/label_encoder.joblib')
+encoder = joblib.load('/home/dhanshree/Desktop/Project_Photos/finance-latest/label_encoder.joblib')
 
 @app.route('/')
 def home():
@@ -21,16 +21,23 @@ def predict():
     try:
         # Get the text input from the HTML form
         text = request.form['text']
+       
 
         # Vectorize the input text using the same TfidfVectorizer used during training
         text_vectorized = vectorizer.transform([text])
 
         # Make predictions
         prediction = model.predict(text_vectorized)[0]
-#         score = model.predict_proba(text_vectorized)[0]
-#         score = [round(float(num),3) for num in score]
+        score = model.predict_proba(text_vectorized)[0]
+        score = [round(float(num),3) for num in score]
         new = encoder.inverse_transform([prediction])[0]
-#         raw = f"{score} {new}"
+        if max(score) < 0.5:
+            new = "Unrelated Inputs, sending for manual review"
+        if new == "Credit reporting, credit repair services, or other personal consumer reports":
+            new = "Credit reporting or credit repair services"
+        if len(text) < 50:
+            new = 'Describe more about finance complaint'
+        #new = f"{new} {score}"
         # Return the prediction as JSON
         return render_template('index.html', prediction=new)
 
